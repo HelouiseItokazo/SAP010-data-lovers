98f3755e34f1bc1a7f8075e0a8b96c89124b40d1
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-05-23 15:42:48 -0300

    Updates

--- data.js
+++ data.js
@@ -1,16 +1,29 @@
-import { buildCard, plotChart } from "./main.js";
 
 export const filterData = (namePokemon, data) => {
 
-  for (let pokemon in data.pokemon) {
+  const pokemons = [];
+
+  for (const pokemon in data.pokemon) {
+
+    namePokemon = namePokemon.toLowerCase();
 
     if (data.pokemon[pokemon].name.includes(namePokemon)) {
 
-      buildCard("pokemons", data.pokemon[pokemon]);
+      pokemons.push(data.pokemon[pokemon]);
 
       if (Object.keys(data.pokemon[pokemon].evolution).filter((key) => key.includes('next-evolution')).length !== 0) {
 
-        return filterData(data.pokemon[pokemon].evolution['next-evolution'][0].name, data);
+        const idNextEvolution = data.pokemon[pokemon].evolution['next-evolution'][0].num;
+
+        for (const evolution in data.pokemon) {
+
+          if (data.pokemon[evolution].num.includes(idNextEvolution)) {
+
+            pokemons.push(data.pokemon[evolution]);
+
+          }//endIf
+
+        }//endFor
 
       }//endIf
 
@@ -18,57 +31,97 @@ export const filterData = (namePokemon, data) => {
 
   }//endFor
 
+  return pokemons;
+
 }//endFilterData
 
-export const sortData = (data, sortBy, orderBy) => {
+const alphabetical = (orderBy, data) => {
 
   if (orderBy === 'a-z') {
 
-    data.pokemon.sort(function (a, b) {
+    data.pokemon.sort((a, b) => {
       if (a.name < b.name) {
         return -1;
       }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
+
     });
 
-  } else if (orderBy === 'z-a') {
-    data.pokemon.sort(function (a, b) {
-      if (a.name < b.name) {
-        return 1;
-      }
+  }
+  if (orderBy === 'z-a') {
+    data.pokemon.sort((a, b) => {
       if (a.name > b.name) {
         return -1;
       }
-      return 0;
+
     });
 
   }//endIf
 
-  for (let pokemon in data.pokemon) {
+}//endAlphabetical
+
+const pokedexNumber = (orderBy, data) => {
+
+  if (orderBy === 'asc') {
+
+    data.pokemon.sort((a, b) => {
+
+      if (a.num < b.num) {
+        return -1;
+      }
+
+    });
+
+  }
+
+  if (orderBy === 'desc') {
+
+    data.pokemon.sort((a, b) => {
+
+      if (a.num > b.num) {
+        return -1;
+      }
+
+    });
+
+  }//endIf
+
+}//endStrength
+
+export const sortData = (data, sortBy, orderBy) => {
+
+  if (orderBy === 'a-z' || orderBy === 'z-a') {
+    alphabetical(orderBy, data);
+  }
+
+  if (orderBy === 'asc' || orderBy === 'desc') {
+    pokedexNumber(orderBy, data);
+  }
+
+  const pokemons = [];
+
+  for (const pokemon in data.pokemon) {
 
     if (data.pokemon[pokemon].type.includes(sortBy)) {
 
-      buildCard("pokemons", data.pokemon[pokemon]);
+      pokemons.push(data.pokemon[pokemon]);
 
     }//endIf
 
-
   }//endFor
+  return pokemons;
 
 }//endSortData
+
 
 export const computeStats = {
 
   calculatePokemonTypesInPercentages: function (data) {
 
-    let totalNumberOfPokemons = data.pokemon.length;
+    const totalNumberOfPokemons = data.pokemon.length;
 
     const count = {};
 
-    for (let object in data.pokemon) {
+    for (const object in data.pokemon) {
 
       data.pokemon[object].type.forEach(type => {
         count[type] = (count[type] || 0) + 1;
@@ -78,12 +131,16 @@ export const computeStats = {
 
     for (const property in count) {
       count[property] = ((count[property] / totalNumberOfPokemons) * 100).toFixed(2);
-      plotChart(property, count[property]);
     }//endFor
+
+    return count;
 
   },//endCalculatePokemonTypesInPercentages
 
-  findTheHeigthPokemon: function (data) {
+
+  findTheTallestPokemon: function (data) {
+
+    const pokemons = [];
 
     const sizes = data.pokemon.map(object => {
       return parseFloat(object.size.height);
@@ -91,23 +148,21 @@ export const computeStats = {
 
     const max = Math.max(...sizes);
 
-    const min = Math.min(...sizes);
+    for (const object in data.pokemon) {
 
-    for(let object in data.pokemon){
-
-      if(data.pokemon[object].size.height.includes(max)){
-        buildCard("pokemonsHeight", data.pokemon[object]);
-      }//endIf
-
-      if(data.pokemon[object].size.height.includes(min)){
-         buildCard("pokemonsHeight", data.pokemon[object]);
+      if (data.pokemon[object].size.height.includes(max)) {
+        pokemons.push(data.pokemon[object]);
+        return pokemons;
       }//endIf
 
     }//endFor
 
   },//endFindTheHeigthPokemon
 
-  findTheWeightPokemon: function (data) {
+
+  findTheHeaviestPokemon: function (data) {
+
+    const pokemons = [];
 
     const sizes = data.pokemon.map(object => {
       return parseFloat(object.size.weight);
@@ -115,16 +170,11 @@ export const computeStats = {
 
     const max = Math.max(...sizes);
 
-    const min = Math.min(...sizes);
+    for (const object in data.pokemon) {
 
-    for(let object in data.pokemon){
-
-      if(data.pokemon[object].size.weight.includes(max)){
-        buildCard("pokemonsWeight", data.pokemon[object]);
-      }//endIf
-
-      if(data.pokemon[object].size.weight.includes(min)){
-         buildCard("pokemonsWeight", data.pokemon[object]);
+      if (data.pokemon[object].size.weight.includes(max)) {
+        pokemons.push(data.pokemon[object]);
+        return pokemons;
       }//endIf
 
     }//endFor
--- main.js
+++ main.js
@@ -1,129 +1,178 @@
 import { computeStats, filterData, sortData } from './data.js';
 import data from './data/pokemon/pokemon.js';
+import stylesPokemon from './stylesPokemon.js';
 
-//Só adiciona o addEventListener se o elemento for acionado
-//Pegando elemento do input
+
 const name = document.getElementById("namePokemon");
+
 if (name) {
   name.addEventListener("input", () => {
     document.getElementById("pokemons").innerHTML = "";
-    const namePokemon = document.getElementById("namePokemon").value;
-    filterData(namePokemon, data);
-  });
+    buildCard(filterData(name.value, data));
+    if (name.value === "") {
+      buildCard(data.pokemon);
+    }//if
+  })//endAddEventListener
 }//endIf
 
-//Só adiciona o addEventListener se o elemento for acionado
-//Pegando elemento do botão pesquisar
 const searchType = document.getElementById("submit");
+
 if (searchType) {
   searchType.addEventListener("click", () => {
     name.value = "";
     document.getElementById("pokemons").innerHTML = "";
     const sortBy = document.getElementById("sortBy").value;
     const orderBy = document.getElementById("orderBy").value;
-    sortData(data, sortBy, orderBy);
-  });
+    buildCard(sortData(data, sortBy, orderBy));
+  });//endAddEventListener
 }//endIf
 
-//Só adiciona o addEventListener se o elemento for acionado
-//Pegando elemento do botão clique para descobrir o peso dos pokemons
-const findPokemonWeight = document.getElementById("findPokemonWeight");
+const findPokemonWeight = document.getElementById("snorlax");
+
+const nameRevealedSnorlax = document.getElementById("nameRevealedSnorlax");
+
 if (findPokemonWeight) {
   findPokemonWeight.addEventListener("click", () => {
-    document.getElementById("pokemonsWeight").innerHTML = "";
-    computeStats.findTheWeightPokemon(data);
+    showImagePokemonAndName(computeStats.findTheHeaviestPokemon(data), findPokemonWeight, nameRevealedSnorlax)
   });
 }//endIf
 
-//Só adiciona o addEventListener se o elemento for acionado
-//Pegando elemento do botão clique para descobrir a altura dos pokemons
-const findPokemonHeight = document.getElementById("findPokemonHeight");
+const findPokemonHeight = document.getElementById("politoed");
+const nameRevealedPolitoed = document.getElementById("nameRevealedPolitoed");
+
+
 if (findPokemonHeight) {
   findPokemonHeight.addEventListener("click", () => {
-    document.getElementById("pokemonsHeight").innerHTML = "";
-    computeStats.findTheHeigthPokemon(data);
+    showImagePokemonAndName(computeStats.findTheTallestPokemon(data), findPokemonHeight, nameRevealedPolitoed)
   });
 }//endIf
 
-//Só adiciona o addEventListener se o elemento for acionado
-//Pegando elemento do botão clique para descobrir os tipos mais raros e comuns dos pokemons
-const findPercentageOfTypesOfPokemons = document.getElementById("findPercentageOfTypesOfPokemons");
-if (findPercentageOfTypesOfPokemons) {
-  findPercentageOfTypesOfPokemons.addEventListener("click", () => {
-    document.getElementById("chart").innerHTML = "";
-    computeStats.calculatePokemonTypesInPercentages(data);
-  });
-}//endIf
+function showImagePokemonAndName(pokemon, tagImg, tagP) {
+  for (const image in pokemon) {
+    tagImg.src = pokemon[image].img;
+    tagP.innerHTML = pokemon[image].name;
+  }
+}
 
-//Função que constrói os cards dos pokémons
-export const buildCard = (id, pokemon) => {
 
-  let pokemons = document.getElementById(id);
-  let cardPokemon = document.createElement('div');
-  cardPokemon.classList.add("card");
+const styleCard = (pokemon, stylesPokemon) => {
 
-  cardPokemon.innerHTML = `
-    <img id="imagePokemon" alt="Image Pokemon" src="${pokemon.img}">
-    <div class="showNamePokemon">
-      <img class="pokeball" alt="Image Pokeball" src="./images/pokeball.png">
-      <h1 id="name">${pokemon.name}</h1>
-    </div>
-    <div class="dataPokemon">
-      <p>Type: <span id="type">${pokemon.type}</span> </p>
-      <p>Height: <span id="type">${pokemon.size.height}</span> </p>
-      <p>Weigth: <span id="type">${pokemon.size.weight}</span> </p>
-    </div>
-   `;
+  for (const data in pokemon) {
 
-  pokemons.appendChild(cardPokemon);
+    Object.defineProperty(pokemon[data], 'imgType', { value: [] });
+    Object.defineProperty(pokemon[data], 'colorType', { value: [] });
+
+    stylesPokemon.forEach(style => {
+
+      if (pokemon[data].type.includes(style.typePokemon)) {
+
+        pokemon[data].imgType.push(style.img);
+        pokemon[data].colorType.push(style.color);
+
+      }//endIf
+
+    })//endForEach
+
+  }//endFor
+
+}//endStyleCard
+
+styleCard(data.pokemon, stylesPokemon);
+
+const buildCard = (pokemon) => {
+
+  document.getElementById("pokemons").innerHTML = "";
+
+  const pokemons = document.getElementById("pokemons");
+
+  console.log(pokemon.length)
+
+  for (let i = 0; i < pokemon.length; i++) {
+
+    console.log(pokemon[i].name)
+
+    if (pokemon[i].colorType.length !== 0 && pokemon[i].imgType.length !== 0) {
+
+      const cardPokemon = document.createElement('div');
+      cardPokemon.classList.add("card");
+      cardPokemon.style.backgroundColor = pokemon[i].colorType[0];
+      let showDataPokemon = `
+    <div class="showDataPokemon">
+      <div class="name-number">
+        <h1 id="name">${pokemon[i].name}</h1>
+        <p id="num">${pokemon[i].num}</p>
+      </div>
+      <div class=all-images-card-pokemon>
+        <div class="img-types">
+    `;
+
+      for (let j = 0; j < pokemon[i].imgType.length; j++) {
+        showDataPokemon += `
+        <img class="img-type" src=${pokemon[i].imgType[j]}>`
+      }
+
+      showDataPokemon += `
+        </div>
+        <img id="imagePokemon" alt="Image Pokemon" src=${pokemon[i].img}>
+      </div>
+    </div>`;
+
+      cardPokemon.innerHTML = showDataPokemon;
+
+      if (pokemons) {
+
+        pokemons.appendChild(cardPokemon);
+
+      }//endIf
+
+    }
+
+  }//endFor
+
 
 }//endBuildCard
-
-//Função que constrói o gráfico exibindo o percentual dos tipos de pokémons
-export const plotChart = (typePokemon, width) => {
-
-  let grafico = document.getElementById("chart");
-  let dados = document.createElement('section');
-
-  dados.innerHTML = `
-    <div id="typeOfPokemon">
-      <div id="tipo">${typePokemon}</div>
-    </div>
-    <div id="percentage">
-      <div id="value" style="width: ${width}% "> ${width}% </div>
-    </div>
-   `;
-
-  grafico.appendChild(dados);
-
-}//endBuildScreen
 
 
 document.querySelectorAll("header .home").forEach(
   item => {
     item.addEventListener("click", () => {
-      console.log("click");
       return window.location = "./";
     })
 
   }
 )
 
-document.getElementById("menuMob").addEventListener("click", () => {
-  const menu = document.querySelector("#navCel");
-  menu.classList.toggle("active");
-})
+const menuMob = document.getElementById("menuMob");
 
-//adicionando evento no carregamento da página
-document.getElementById("pokemons").addEventListener("load", onload(data));
+if (menuMob) {
+  menuMob.addEventListener("click", () => {
+    const menu = document.querySelector("#navCel");
+    menu.classList.toggle("active");
+  })
+}
 
-function onload(data) {
+let url = document.URL;
 
-  for (let pokemon in data.pokemon) {
-    buildCard("pokemons", data.pokemon[pokemon]);
-  }//enFor
+if (url === "http://localhost:3000/") {
+  window.addEventListener("load", () => {
+    const home = document.querySelector(".home");
+    home.style.color = "#00478C"
+    buildCard(data.pokemon);
+  });//endAddEventListener
+}
 
-}//endOnload
+if (url === "http://localhost:3000/statistic") {
+  const curiosidades = document.querySelector(".curiosidades");
+  curiosidades.style.color = "#00478C"
+}
+
+
+
+
+
+
+
+
 
 
 
